897a3b22ba9e2fc7aebf5293666676bf7f0eafc2
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-24 22:07:22 -0300

    Just practicing a bit.

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -2,4 +2,15 @@ package cinema
 
 fun main() {
     // write your code here
+    println("Enter the number of rows:")
+    val rows = readLine()!!.toInt()
+    println("Enter the number of seats in each row:")
+    val seats = readLine()!!.toInt()
+    println("Total income:")
+    if (rows * seats <= 60) {
+        println("$${rows * seats * 10}")
+    } else {
+        println("$${(rows / 2 * seats * 10) + (rows - rows / 2) * seats * 8}")
+    }
+
 }
