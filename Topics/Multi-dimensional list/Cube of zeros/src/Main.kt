6299a7fde6b76a606a81b955b8c33a733b38bc5d
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-28 13:34:19 -0300

    Just practicing a bit.

---
+++ Topics/Multi-dimensional list/Cube of zeros/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //write your code here
+    val list3D = mutableListOf(
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0)),
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0)),
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0))
+    )
+    println(list3D)
+}
