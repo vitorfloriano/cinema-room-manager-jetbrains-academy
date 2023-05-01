8c7186a5b62d8084153c10db4d0349856d931f7c
vitorfloriano <vitorflorianoc@gmail.com>
2023-04-30 21:05:37 -0300

    Just practicing a bit.

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -2,17 +2,56 @@ package cinema
 
 fun main() {
     // write your code here
-
     println("Enter the number of rows:")
     val rows = readLine()!!.toInt()
     println("Enter the number of seats in each row:")
     val seats = readLine()!!.toInt()
-    println("Total income:")
-    if (rows * seats <= 60) {
-        println("$${rows * seats * 10}")
-    } else {
-        println("$${(rows / 2 * seats * 10) + (rows - rows / 2) * seats * 8}")
-    }
 
+    println("Cinema:")
+    print(" ")
+    for (i in 1..seats) {
+        print(" $i")
+    }
+    println()
+    for (i in 1..rows) {
+        print("$i")
+        for (j in 1..seats) {
+            print(" S")
+        }
+        println()
+    }
+    println("Enter a row number:")
+    val row = readLine()!!.toInt()
+    println("Enter a seat number in that row:")
+    val seat = readLine()!!.toInt()
+    println("Ticket price: ")
+    if (rows * seats <= 60) {
+        println("$10")
+    } else {
+        if (row <= rows / 2) {
+            println("$10")
+        } else {
+            println("$8")
+        }
+    }
+    println()
+    println("Cinema:")
+    print(" ")
+    for (i in 1..seats) {
+        print(" $i")
+    }
+    println()
+    for (i in 1..rows) {
+        print("$i")
+        for (j in 1..seats) {
+            if (i == row && j == seat) {
+                print(" B")
+            } else {
+                print(" S")
+            }
+        }
+        println()
+    }
+    println()
 
 }
