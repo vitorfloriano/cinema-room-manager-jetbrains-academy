cbb8d0404a16390d211170f70d99b597ecb99de5
vitorfloriano <vitorflorianoc@gmail.com>
2023-05-10 23:19:57 -0300

    Just practicing a bit.

---
+++ Topics/The try-catch-finally statement/The end/src/Main.kt
@@ -0,0 +1,13 @@
+fun solution() {
+    // write here
+    val num1 = readln()!!.toInt()
+    val num2 = readln()!!.toInt()
+
+    try {
+        println(num1 / num2)
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        println("This is the end, my friend.")
+    }
+}
